3f68c0edce056daaa214850f9f280cf9649ccbff
reistence <gvlamberti@gmail.com>
2022-10-12 19:12:11 +0200

    thumbnail implemented

--- js/script.js
+++ js/script.js
@@ -9,31 +9,33 @@ const imgArray = [
 
 // container
 const container = document.querySelector(".items-container");
+//thumbnails
+const thumbnails = document.getElementById("thumbnails");
 
 for (let i = 0; i <= imgArray.length - 1; i++) {
   const currentImg = imgArray[i];
   const element = `<div class="item">
             <img src="${currentImg}" alt="" />
           </div>`;
+  const thumbElement = `<img class="thumb-img" src=${currentImg} alt=""/>`;
   container.innerHTML += element;
+  thumbnails.innerHTML += thumbElement;
 }
 
-// Starting point
+// Starting point img primary
 const items = document.getElementsByClassName("item");
 let sliderCurrentPosition = 0;
 items[sliderCurrentPosition].classList.add("active");
+//startingpoint thumbnails
+const thumbImg = document.getElementsByClassName("thumb-img");
+thumbImg[sliderCurrentPosition].classList.add("active-thumb");
+
 // grab html elements
 // btn
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 
-//thumbnails
-// const thumbnails = document.getElementById("thumbnails");
-// const thumbImg = document.getElementsByClassName("thumb-img");
-// console.log(thumbImg);
-
 // Navigation
-
 // next btn
 nextBtn.addEventListener("click", function () {
   console.log(sliderCurrentPosition);
@@ -60,24 +62,30 @@ nextBtn.addEventListener("click", function () {
   //   console.log(sliderCurrentPosition);
 
   items[sliderCurrentPosition].classList.remove("active");
+  thumbImg[sliderCurrentPosition].classList.remove("active-thumb");
+
   if (sliderCurrentPosition < imgArray.length - 1) {
     sliderCurrentPosition++;
   } else {
     sliderCurrentPosition = 0;
   }
   items[sliderCurrentPosition].classList.add("active");
+  thumbImg[sliderCurrentPosition].classList.add("active-thumb");
 });
 
 // prev btn
 prevBtn.addEventListener("click", function () {
   console.log(sliderCurrentPosition);
   items[sliderCurrentPosition].classList.remove("active");
+  thumbImg[sliderCurrentPosition].classList.remove("active-thumb");
+
   if (sliderCurrentPosition > 0) {
     sliderCurrentPosition--;
   } else {
     sliderCurrentPosition = imgArray.length - 1;
   }
   items[sliderCurrentPosition].classList.add("active");
+  thumbImg[sliderCurrentPosition].classList.add("active-thumb");
 
   //   if (sliderCurrentPosition > 0) {
   //     // remove active from current img
